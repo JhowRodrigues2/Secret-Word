56062fa7e024b68e7122aebaa52d8ac110cf8c74
Jhow Rodrigues <jhowrdev@gmail.com>
2023-08-14 10:53:50 -0300

    initial css configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
-
 import './App.css'
-
+import videoBG from './assets/video_bg.mp4'
 function App() {
 
   return (
-    <>
-     
-    </>
+    <div className="App video-background">
+        <video autoPlay muted loop>
+            <source src={videoBG} type="video/mp4"/>
+            Seu navegador não suporta vídeos em HTML5.
+        </video>
+        <div className="content">
+          
+        </div>
+    </div>
   )
 }
 
