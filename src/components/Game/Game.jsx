ed329d3f3cca086d9bbdf975e1ff00e2bd629af7
Jhow Rodrigues <jhowrdev@gmail.com>
2023-08-19 09:22:52 -0300

    consuming the states

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,28 +1,47 @@
 import React from "react";
 import "./Game.css";
-const Game = ({ verifyLetter }) => {
+const Game = ({
+  verifyLetter,
+  picketWord,
+  pickedCategory,
+  letters,
+  guessedLetters,
+  wrongLetters,
+  guesses,
+  score,
+}) => {
   return (
     <div className="game">
       <p className="points">
-        <span>Pontuaçao:000</span>
+        <span>Pontuaçao:{score}</span>
       </p>
       <h1>Adivinhe a palavra:</h1>
       <h3 className="tip">
-        Dica sobre a palavra: <span>Dica...</span>
+        Dica sobre a palavra: <span>{pickedCategory}</span>
       </h3>
-        <div className="wordContainer">
-          <span className="letter">A</span>
-          <span className="BlankSquare"></span>
-        </div>
-    <div className="letterContainer">
-      <input type="text" name="letter" id="" maxLength="1" required />
-      <button>Jogar!</button>
-    </div>
-    <div className="wrongLettersContainer">
-      <p>Letras já utilizadas:</p>
-      <span>B</span>
-      <span>A</span>
-    </div>
+      <p>Você ainda tem {guesses} tentativa(s).</p>
+      <div className="wordContainer">
+        {letters.map((letter, index) =>
+          guessedLetters.includes(letter) ? (
+            <span key={index} className="letter">
+              {letter}
+            </span>
+          ) : (
+            <span key={index} className="blankSquare"></span>
+          )
+        )}
+      </div>
+      <div className="letterContainer">
+        <input type="text" name="letter" id="" maxLength="1" required />
+        <button>Jogar!</button>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+
+        {wrongLetters.map((letter, index) => (
+          <span key={index}>{letter}</span>
+        ))}
+      </div>
     </div>
   );
 };
